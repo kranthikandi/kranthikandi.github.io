b201964f14d4e29a97d29d6e49aad9e3d72ada1b
kranthikandi <kranthikumar.kandi@gmail.com>
2015-10-30 13:49:24 -0700

    test

---
+++ src/com/kk/Test.java
@@ -0,0 +1,15 @@
+package com.kk;
+
+public class Test {
+	public String name(){
+		String name = "kranthi";
+		return name;
+	}
+
+	public static void main(String[] args) {
+		
+		Test test= new Test();
+		test.name();
+	}
+
+}
