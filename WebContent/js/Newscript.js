7b94308a1c569315745db368db493defcfd71cf1
kranthikandi <kranthikumar.kandi@gmail.com>
2015-11-02 09:53:46 -0800

    test

--- WebContent/js/Newscript.js
+++ WebContent/js/Newscript.js
@@ -1,33 +1,24 @@
-/**
- * 
- *//*!
- * jQuery Cookie Plugin v1.3.1
- * https://github.com/carhartl/jquery-cookie
- *
- * Copyright 2013 Klaus Hartl
- * Released under the MIT license
- */
-(function(factory){if(typeof define==='function'&&define.amd){define(['jquery'],factory);}else{factory(jQuery);}}(function($){var pluses=/\+/g;function raw(s){return s;}function decoded(s){return decodeURIComponent(s.replace(pluses,' '));}function converted(s){if(s.indexOf('"')===0){s=s.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,'\\');}try{return config.json?JSON.parse(s):s;}catch(er){}}var config=$.cookie=function(key,value,options){if(value!==undefined){options=$.extend({},config.defaults,options);if(typeof options.expires==='number'){var days=options.expires,t=options.expires=new Date();t.setDate(t.getDate()+days);}value=config.json?JSON.stringify(value):String(value);return(document.cookie=[config.raw?key:encodeURIComponent(key),'=',config.raw?value:encodeURIComponent(value),options.expires?'; expires='+options.expires.toUTCString():'',options.path?'; path='+options.path:'',options.domain?'; domain='+options.domain:'',options.secure?'; secure':''].join(''));}var decode=config.raw?raw:decoded;var cookies=document.cookie.split('; ');var result=key?undefined:{};for(var i=0,l=cookies.length;i<l;i++){var parts=cookies[i].split('=');var name=decode(parts.shift());var cookie=decode(parts.join('='));if(key&&key===name){result=converted(cookie);break;}if(!key){result[name]=converted(cookie);}}return result;};config.defaults={};$.removeCookie=function(key,options){if($.cookie(key)!==undefined){$.cookie(key,'',$.extend({},options,{expires:-1}));return true;}return false;};}));
+// The root URL for the RESTful services
+var rootURL = "http://localhost:8080/cellar/rest/wines";
 
-function setCookie() {
-  $.cookie("visited", "true", { expires: 365 });
-}
+var currentWine;
 
-if ($.cookie('visited') != 'true') {
-  $('#welcome, .overlay').show(100);
-  setCookie();
-} else {
-  $('#welcome').remove();
-}
+$(window).load(function() {
+	 // executes when complete page is fully loaded, including all frames, objects and images
+	 //alert("window is loadedzzzzzzzzz");
+	 
+	});
 
-$('#sidebar').click(function() {
-  $('#welcome').hide(100);
-    $('.overlay').fadeOut(100);
-});
 
-$('#sidebar').mouseover(function(){
-	
-	$('.overlay').fadeOut(100);
-});
+var person = {
+		  firstName: "Jim",
+		  introduce: function(){
+			  console.log("Hi, I'm " + this.firstName);
+		  }
+		};
 
-// $.cookie("visited", null);
+		person.introduce();
+		// Outputs: Hi, I'm Jim
+		setTimeout(person.introduce, 20);
+
+//alert("window loadad");
